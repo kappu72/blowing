c77e76a37250c1032ecb061d59e48c51eb930d2f
Andrea Cappugi <kappu72@gmail.com>
2019-12-18 12:40:47 +0100

    Fixin daniele index

--- services/getStationData.php
+++ services/getStationData.php
@@ -2,7 +2,10 @@
 $url="http://dev.retemet.com:3333/station/st_19070267/last";
 
 $data = file_get_contents($url);
+$data = $data != false ? $data : [];
+
 $dataMax = file_get_contents($url."max");
+
 header("Access-Control-Allow-Origin: *");
 header("Content-type:application/json; charset=utf-8");
 echo "{ last:".$data. "max: ".$dataMax."}";
