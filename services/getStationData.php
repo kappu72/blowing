531e9013899a8f3b77dd2cd3ff93882eafaae558
root <root@sl01.migrazioni.it>
2019-12-18 22:28:11 +0100

    Added last hour max to getStationData

--- services/getStationData.php
+++ services/getStationData.php
@@ -6,8 +6,11 @@ $data = $data != false ? $data : [];
 
 $dataMax = file_get_contents($url."max");
 
+$res=array( 'last' => json_decode($data), 'max' => json_decode($dataMax));
+
+
 header("Access-Control-Allow-Origin: *");
 header("Content-type:application/json; charset=utf-8");
-echo "{ last:".$data. "max: ".$dataMax."}";
+echo json_encode($res);
 
 ?>
