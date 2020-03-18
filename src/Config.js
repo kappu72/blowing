1b54cbff08a3ad2426d546bd97983cb122c46bce
Andrea Cappugi <kappu72@gmail.com>
2020-03-18 22:19:42 +0100

    miglioramenti multi stazione aggiunto anche chart progest configurabile

--- src/Config.js
+++ src/Config.js
@@ -1,13 +1,16 @@
-const config = {
+let config = {
     st_19070267: { topic: "device/ELP001/19070267/metrics/#",
-    title: "PENDENNIS SHIPYARD <br/>50&deg;09&#39;13&#34; N 5&deg;03&#39;09&#34; W<br/>Real Time Wind Data"
+    title: "PENDENNIS SHIPYARD <br/>50&deg;09&#39;13&#34; N 5&deg;03&#39;09&#34; W<br/>Real Time Wind Data",
+    iframe: "https://navimeteoexternals-dev.progestnow.com/externals/navimeteo/forecast/onehforecast?port_code=GBLGP&lat=50.1540592&lng=-5.052021"
     },
     st_19090456: { topic: "device/ALP001/19090456/metrics/#",
-    title: "TODO change PENDENNIS SHIPYARD <br/>50&deg;09&#39;13&#34; N 5&deg;03&#39;09&#34; W<br/>Real Time Wind Data"
+    title: "TODO change PENDENNIS SHIPYARD <br/>50&deg;09&#39;13&#34; N 5&deg;03&#39;09&#34; W<br/>Real Time Wind Data",
+    iframe: "https://navimeteoexternals-dev.progestnow.com/externals/navimeteo/forecast/onehforecast?port_code=GBLGP&lat=50.1540592&lng=-5.052021"
     }
   }
-const parsedUrl = new URL(window.location.href);
-const id = parsedUrl.searchParams.get("id") || "19070267"
-const st_id = "st_" + id;
+let parsedUrl = new URL(window.location.href);
+let _id = parsedUrl.searchParams.get("id") || "19070267"
+let st_id = "st_" + _id;
+st_id = config[st_id] ? st_id : "st_19070267"
 //Settings the station title
 document.querySelector("#title").innerHTML = config[st_id].title;
