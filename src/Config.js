8bdaa0bec29b85f0e041892a7d60c16fa5f24fcb
root <root@sl01.migrazioni.it>
2020-04-30 12:59:41 +0200

    Aggiunte modifiche Daniele da server

--- src/Config.js
+++ src/Config.js
@@ -3,13 +3,13 @@ let config = {
 	title: "PENDENNIS SHIPYARD | Real Time Wind Data",
     logo: "<a href='https://pendennis.com/' target='_blank'><img src='img/pendennis.png' alt='logo customer'></a> ", 
     info: "PENDENNIS SHIPYARD <br/>50&deg;09&#39;13&#34; N 5&deg;03&#39;09&#34; W<br/>Real Time Wind Data",
-    iframe: "https://navimeteoexternals-dev.progestnow.com/externals/navimeteo/forecast/onehforecast?port_code=GBLGP&lat=50.138985&lng=-5.033733"
+    iframe: "https://navimeteoexternals.progestnow.com/externals/navimeteo/forecast/onehforecast?port_code=GBLGP&lat=50.138985&lng=-5.033733"
     },
     st_19090456: { topic: "device/ALP001/19090456/metrics/#",
     title: "NCI Cape Cornwall | Real Time Wind Data",
     logo: "<a href='https://www.nci.org.uk/capecornwall' target='_blank'><img src='img/nci.png' alt='logo customer'></a> ", 
 	info: "CAPE CORNWALL Lookout Station <br/>50&deg;07.63&#39;N - 5&deg;42.56&#39;W<br/>Real Time Wind Data",    
-    iframe: "https://navimeteoexternals-dev.progestnow.com/externals/navimeteo/forecast/onehforecast?port_code=GBCCL&lat=50.126992&lng=-5.715079"  
+    iframe: "https://navimeteoexternals.progestnow.com/externals/navimeteo/forecast/onehforecast?port_code=GBCCL&lat=50.126992&lng=-5.715079"  
     } 
   }
 let parsedUrl = new URL(window.location.href);
