e138e82dbe9452dcb5c291a1647f14a70204e5a7
root <root@sl01.migrazioni.it>
2020-06-10 12:46:00 +0200

    Aggiunte modifiche Daniele

--- src/Config.js
+++ src/Config.js
@@ -8,23 +8,26 @@ let config = {
     st_19070267: { 
         id: "st_19070267",
         topic: "device/ALP001/19070267/metrics/#",
-	    title: "<i class='material-icons'>place</i> PENDENNIS SHIPYARD | Weather Station",                
-        logo: "<a href='https://pendennis.com/' target='_blank'><img src='img/pendennis.png' alt='logo customer'></a> ", 
+	    title: "<i class='material-icons'>place</i> PENDENNIS SHIPYARD | Weather Station",  
+	    titlePage: "PENDENNIS SHIPYARD | Real Time Wind Data",     
+        logo: "<a href='https://pendennis.com/' target='_blank'><img src='img/pendennis.png' alt='logo customer'></a> ",             
         info: "PENDENNIS SHIPYARD <br/>50&deg;09&#39;13&#34; N 5&deg;03&#39;09&#34; W<br/>Real Time Wind Data",
         iframe: "https://navimeteoexternals.progestnow.com/externals/navimeteo/forecast/onehforecast?port_code=GBLGP&lat=50.138985&lng=-5.033733"
     },
     st_19090456: { 
         id: "st_19090456",
         topic: "device/ALP001/19090456/metrics/#",
-        title: "NCI Cape Cornwall | Real Time Wind Data",
-        logo: "<a href='https://www.nci.org.uk/capecornwall' target='_blank'><img src='img/nci.png' alt='logo customer'></a> ", 
+        title: "<i class='material-icons'>place</i> NCI Cape Cornwall | Real Time Wind Data",
+		titlePage: "NCI Cape Cornwall | Real Time Wind Data",       
+	    logo: "<a href='https://www.nci.org.uk/capecornwall' target='_blank'><img src='img/nci.png' alt='logo customer'></a> ", 
 	    info: "CAPE CORNWALL Lookout Station <br/>50&deg;07.63&#39;N - 5&deg;42.56&#39;W<br/>Real Time Wind Data",    
         iframe: "https://navimeteoexternals.progestnow.com/externals/navimeteo/forecast/onehforecast?port_code=GBCCL&lat=50.126992&lng=-5.715079"
     },
     st_19090456M: { 
         id: "st_19090456",
         topic: "device/ALP001/19090456/metrics/#",
-        title: "NCI Cape Cornwall | Real Time Wind Data",
+        title: "<i class='material-icons'>place</i> NCI Cape Cornwall | Real Time Wind Data",               
+		titlePage: "NCI Cape Cornwall | Real Time Wind Data",    
         logo: "<a href='https://www.nci.org.uk/capecornwall' target='_blank'><img src='img/nci.png' alt='logo customer'></a> ", 
 	    info: "CAPE CORNWALL Lookout Station <br/>50&deg;07.63&#39;N - 5&deg;42.56&#39;W<br/>Real Time Wind Data",    
         iframe: "https://navimeteoexternals.progestnow.com/externals/navimeteo/forecast/onehforecast?port_code=GBCCL&lat=50.126992&lng=-5.715079",
@@ -37,6 +40,8 @@ let st_id = "st_" + _id;
 st_id = config[st_id] ? st_id : "st_19070267"
 //Settings the station title
 setInnerHTML("#title", config[st_id].title);
+setInnerHTML("#titlePage", config[st_id].titlePage);                    
 setInnerHTML("#info", config[st_id].info);
-setInnerHTML("#logo", config[st_id].logo);  
+setInnerHTML("#logo", config[st_id].logo);       
+setInnerHTML("#logo", config[st_id].logo);        
       
