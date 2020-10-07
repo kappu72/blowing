b39f9591e84cdfae21932420270adf20a8f9e525
root <root@sl01.migrazioni.it>
2020-10-07 15:26:49 +0200

    Aggiunta stazione st_19100250

--- src/Config.js
+++ src/Config.js
@@ -32,7 +32,19 @@ let config = {
 	    info: "CAPE CORNWALL Lookout Station <br/>50&deg;07.63&#39;N - 5&deg;42.56&#39;W<br/>Real Time Wind Data",    
         iframe: "https://navimeteoexternals.progestnow.com/externals/navimeteo/forecast/onehforecast?port_code=GBCCL&lat=50.126992&lng=-5.715079",
         uom: "mph" // uom: one of kt, ms, mph default kt
-    } 
+    } ,
+
+    st_19100250: { 
+        id: "st_19100250",
+        topic: "device/ALP001/19100250/metrics/#",
+        title: "<i class='material-icons'>place</i> Torre Marina Chiavari | Real Time Wind Data",               
+		titlePage: "Torre Marina Chiavari | Real Time Wind Data",    
+        logo: "<a href='https://www.retemet.com/' target='_blank'><img src='img/retemet.png' alt='logo customer'></a> ",    
+	    info: "Torre Marina Chiavari <br/>44&deg;18&#39;N - 09&deg;19&#39;E<br/>Real Time Wind Data",      
+        iframe: "https://navimeteoexternals.progestnow.com/externals/navimeteo/forecast/onehforecast?port_code=ITCHX&lat=44.312604&lng=9.320266",         
+    }      
+	     
+	
   }
 let parsedUrl = new URL(window.location.href);
 let _id = parsedUrl.searchParams.get("id") || "19070267"
