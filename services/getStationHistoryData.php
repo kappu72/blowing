3a19962b193fd1953f1469f4239fac4eb8d88fe4
Andrea Cappugi <kappu@rotolo.lan>
2021-01-17 07:15:44 +0100

    Fixed typo

--- services/getStationHistoryData.php
+++ services/getStationHistoryData.php
@@ -3,9 +3,9 @@ $id = $_GET['id'];
 $idro = $_GET['idro'];
 
 if(isset($idro)) {
-  $url="http://dev.retemet.com:3333/station/$id/idrometro";}
-else {
-  $url="http://dev.retemet.com:3333/station/$id/hourly
+  $url="http://dev.retemet.com:3333/station/$id/idrometro";
+}else {
+  $url="http://dev.retemet.com:3333/station/$id/hourly"
 }
 $data = file_get_contents($url);
 $data = $data != false ? $data : [];
