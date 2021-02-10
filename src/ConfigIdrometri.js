ac0380ef8d33d7c2d5c8d0102e3c9472aa65cfb4
root <root@sl01.migrazioni.it>
2021-02-10 11:40:17 +0100

    Configurazione idrometri Daniele

--- src/ConfigIdrometri.js
+++ src/ConfigIdrometri.js
@@ -15,20 +15,20 @@ const config = {
     transform: (punti) => (punti / 1000) + 184.17,
     uom: 'm',
     uomLabel: '(m) s.l.m.',
-    chartCfg: {name: 'Idrometro Comune di Baveno', axisTitle:'Livello idrometrico (m, s.l.m.)'}
+    chartCfg: {name: 'Lago Maggiore | Comune di Baveno', axisTitle:'Livello idrometrico (m, s.l.m.)'}   
 },
 st_sml:  {    
     id: "st_sml",
     topic: "device/TRF001/sml/metrics/#",
-    title: "<i class='material-icons'>place</i> Baveno Idrometro | Real Time Data",                       
-    titlePage: "Baveno Idrometro  | Real Time Data",    
+    title: "<i class='material-icons'>place</i> Torrente S.Siro SML Idrometro | Real Time Data",                       
+    titlePage: "Torrente S.Siro  | Santa Margherita Ligure",    
     logo: "<a href='https://www.retemet.com' target='_blank'><img src='img/retemet.png' alt='logo customer'></a> ",         
-    info: "Idrometro Lago Maggiore <br/>44&deg;54&#39;N - 08&deg;30&#39;E<br/>Dati in tempo reale",       
-    iframe: "https://navimeteoexternals.progestnow.com/externals/navimeteo/forecast/onehforecast?port_code=ITCHX&lat=44.312604&lng=9.320266", 
-    transform: (punti) => ((punti - 4686) / 10),
-    uom: 'cm',
-    uomLabel: '(cm)',
-    chartCfg: {name: 'Idrometro Comune di Santa Margherita Ligure', axisTitle:'Livello idrometrico (cm)'}
+    info: "Torrente S.Siro <br/>44&deg;54&#39;N - 08&deg;30&#39;E<br/>Dati in tempo reale",        
+    iframe: "https://navimeteoexternals.progestnow.com/externals/navimeteo/forecast/onehforecast?port_code=ITCHX&lat=44.312604&lng=9.320266",  
+    transform: (punti) => ((punti - 4686) / 1000),
+    uom: 'm',
+    uomLabel: '(m)',
+    chartCfg: {name: 'Torrente S.Siro Comune di Santa Margherita Ligure', axisTitle:'Livello idrometrico (m)'}          
   }  
 }     
 let parsedUrl = new URL(window.location.href);
@@ -42,5 +42,5 @@ setInnerHTML("#titlePage", config[st_id].titlePage);
 setInnerHTML("#info", config[st_id].info);
 setInnerHTML("#logo", config[st_id].logo);
 setInnerHTML("#uom_label", config[st_id].uomLabel);  
-setInnerHTML("#uom_label_max", config[st_id].uomLabel);  
-setInnerHTML("#uom_label_min", config[st_id].uomLabel);     
+setInnerHTML("#uom_label_max", config[st_id].uomLabel);   
+setInnerHTML("#uom_label_min", config[st_id].uomLabel);         
